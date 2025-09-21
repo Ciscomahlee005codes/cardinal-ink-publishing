5f779c651f1745d40fe39b2904037d1c11949ac1
kiddo9 <dkido913@gmail.com>
2025-09-21 15:41:18 +0100

    security created

--- server/src/controllers/booksController.ts
+++ server/src/controllers/booksController.ts
@@ -1,5 +1,6 @@
 const { books } = require("../../models/indexs");
 const upload = require("../middleware/uploads");
+const fs = require("fs");
 
 exports.createNewBook = [
   upload.array("fileContent", 2),
@@ -88,5 +89,118 @@ exports.Books = async (req: any, res: any) => {
   } catch (error) {
     console.log(error);
     res?.json({ status: false, message: "internal server error" });
+  }
+};
+
+exports.editBook = [
+  upload.array("fileContent", 2),
+  async (req: any, res: any) => {
+    try {
+      const { bookid } = req.query;
+      const { title, author, description, price, cover_url, content_url } =
+        req?.body;
+      const file = req?.files;
+
+      if (!bookid) {
+        return res?.json({ status: false, message: "missing arguments" });
+      }
+
+      if (!Array.isArray(file)) {
+        return res?.json({
+          status: false,
+          message:
+            "file format wrong. ensure the first file is the cover book and the second is the content",
+        });
+      }
+
+      if (
+        (!title ||
+          !author ||
+          !description ||
+          !price ||
+          !cover_url ||
+          !content_url) &&
+        (!file || file.length == 0)
+      ) {
+        return res?.json({
+          status: false,
+          message: "missing arguments. you should have atleast one data",
+        });
+      }
+
+      const checkIfExists = await books.findOne({ where: { id: bookid } });
+
+      if (!checkIfExists) {
+        return res?.json({ status: false, message: "invalid request" });
+      }
+
+      if (file.length > 0) {
+        //don't forget
+      }
+
+      await books.update({
+        title,
+        author,
+        description,
+        price,
+        cover_url,
+        content_url,
+      });
+
+      return res?.json({
+        status: true,
+        message: "book has been updated",
+      });
+    } catch (error) {
+      console.log(error);
+      return res?.json({ status: false, message: "internal server error" });
+    }
+  },
+];
+
+exports.deleteBook = async (req: any, res: any) => {
+  const { bookid } = req.query;
+
+  try {
+    if (!bookid) {
+      return res?.json({ status: false, message: "missing arguments" });
+    }
+
+    const checkIfExists = await books.findOne({ where: { id: bookid } });
+
+    if (!checkIfExists) {
+      return res?.json({ status: false, message: "invalid request. unknown" });
+    }
+
+    const filePath = [checkIfExists.cover_url, checkIfExists.content_url];
+
+    filePath.forEach((path: string) => {
+      fs.unlink(path, (err: any) => {
+        if (err) {
+          console.log(err);
+          return res?.json({
+            status: false,
+            message: "request could not be completed. something went wrong",
+          });
+        }
+      });
+    });
+
+    const deleteBook = await books.destroy({ where: { id: bookid } });
+
+    if (!deleteBook) {
+      return res?.json({
+        status: false,
+        message: "unable to complete your request",
+      });
+    }
+
+    return res?.json({
+      status: true,
+      message: `The book ${checkIfExists.title} has been deleted`,
+    });
+  } catch (error) {
+    console.log(error);
+    return res?.json({ status: false, message: "internal server error" });
   }
 };
--- server/src/routes/routers.ts
+++ server/src/routes/routers.ts
@@ -1,7 +1,17 @@
 const express = require("express");
 const routes = express.Router();
-const { createNewBook } = require("../controllers/booksController");
+const {
+  createNewBook,
+  allBooks,
+  Books,
+  editBook,
+  deleteBook,
+} = require("../controllers/booksController");
 
 routes.post("/createnew/books", createNewBook);
+routes.get("/books", allBooks);
+routes.get("/books/:id", Books);
+routes.put("/book/edit", editBook);
+routes.put("/book/delete", deleteBook);
 
 module.exports = routes;
