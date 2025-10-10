c46fafc39f9c761b5953fbfccecaf6385d20ceff
De Boy Programmer <ciscomahlee@gmail.com>
2025-10-10 07:54:08 +0100

    Thank You Jesus

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ["806d57cae566.ngrok-free.app"], // ✅ no https:// or trailing slash
+    allowedHosts: ["https://wholesome-forgiveness-production.up.railway.app"], // ✅ no https:// or trailing slash
     port: 5173,
   },
 });
