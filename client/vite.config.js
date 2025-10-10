9fa5b52a71cb93de727d8925ff50c653a6b9cf62
kiddo9 <dkido913@gmail.com>
2025-10-10 08:18:20 +0100

    it will now work

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: ["https://earnest-imagination-production-eed6.up.railway.app"], // ✅ no https:// or trailing slash
-    port: 5173,
-  },
 });
