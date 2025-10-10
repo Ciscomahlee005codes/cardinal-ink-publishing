86b60e7b32ff2b76176ac14a93d3480074ad0334
De Boy Programmer <ciscomahlee@gmail.com>
2025-10-10 08:13:14 +0100

    Thank You Jesus

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ["https://wholesome-forgiveness-production.up.railway.app"], // ✅ no https:// or trailing slash
+    allowedHosts: ["https://earnest-imagination-production-eed6.up.railway.app"], // ✅ no https:// or trailing slash
     port: 5173,
   },
 });
